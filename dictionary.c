8da4cf173d8a93faf2cd5bf76de62a911b293066
Ganellon <trimix@gmail.com>
2016-05-22 15:14:27 -0400

    Add files via upload

--- dictionary.c
+++ dictionary.c
@@ -7,7 +7,6 @@
  * Implements a dictionary's functionality.
  */
 #define _GNU_SOURCE // for rawmemchr - GNU extension
-//#include <sys/types.h>
 #include <sys/stat.h> // for stat struct
 #include <time.h>
 #include <stdio.h>
@@ -15,69 +14,77 @@
 #include <string.h> // for memchr and strcmp
 #include <stdint.h> // for murmur hash
 #include "murmur3.h" // murmurhash 3
-#include <pthread.h>
-#include <stdbool.h>
 #include "dictionary.h"
+#include <stdbool.h>
+#include <pthread.h> // todo
 
-//#include <limits.h>
-//#include <ctype.h>
+// for bitwise OR to lowercase
 #define BITS 32
 
+// Some hash sizes I tried to get the fewest collisions
 //#define HASH_SIZE 858546
 //#define HASH_SIZE 1001637
 //#define HASH_SIZE 1287819
 //#define HASH_SIZE 1430910
 //#define HASH_SIZE 5008185
 #define HASH_SIZE 1287821 // try it with a prime
-#define ACTUAL_SIZE 143091
 
+
+// this is the linked list node struct
 typedef struct ChainLink
 {
     uint32_t value;
-    char key[45];
+    char key[45]; // longest word in English dictionary
     struct ChainLink* next;
 } chain_link;
 
-chain_link* CreateLink(uint32_t val, const char * key);
-chain_link* AddLink(chain_link* head, uint32_t val, const char * key);
-// fstat example from fstat man page
-// memchr example from memchr man page
+// Prototypes - create a new link, and add a link to an existing chain, free all
+chain_link* CreateLink(uint32_t val, char * key);
+chain_link* AddLink(chain_link* head, uint32_t val, char * key);
+void destroy(chain_link* begin);
 
-//chain_link Start;
+
+// Trav is a pointer to chain_link, used to "walk" the linked lists
 chain_link *Trav;
-chain_link *UniTrav;
+
+// the actual hash table - yes, it's on the stack even though it's large
 chain_link* hashtable[HASH_SIZE] = {NULL};
 
-chain_link* unique_words[HASH_SIZE] = {NULL};
-chain_link* unique_misspellings[HASH_SIZE] = {NULL};
-unsigned int uniques = 0;
-
-unsigned long actual[ACTUAL_SIZE] = {0};
-int actual_counter = 0;
-
-
+// seed value for murmurhash / 71 had fewest collisions
 int seed = 71;
 //47 = 9713
 //2 = 9676
 //3 = 9590
 // 71 is main number
 
+// counter is an integer that holds the number of words loaded from dictionary
 unsigned int counter = 0;
-//int collisions = 0;
-void destroy(chain_link* begin);
-
-//int* hashes;
-
-//int loader(void);
 
 
+/** This is the load function. It is called by speller.c and is the timed
+ * function that loads words from the dictionary into whatever data structure
+ * the programmer decides to implement. I chose hash table. Dictionary is
+ * an unvalidated string to a path where the dictionary file is located.
+ * 
+ */
 bool load (const char* dictionary)
 {
-    //int seed = 71;
+    
+    // hash is the return value from the murmur hash function
+    // it may seem odd that it's a single element array, but that is because
+    // I was switching back and forth between the 32 bit and 64 bit versions
+    // and the 64 bit version returns 2 64 bit values.
+    // It ends up the 32 bit version, though slightly slower, makes more sense
+    // because the single value is faster to validate than both 64 bit values
     uint32_t hash[1];
+    
+    // the index in the hash table array where values will be stored
     int index = 0;
+    
+    // stat structure for getting information about the dictionary file
     struct stat test_stat;
     
+    // the actual file to read from
     FILE* fp = fopen(dictionary, "r");
     if (fp == NULL)
     {
@@ -85,62 +92,87 @@ bool load (const char* dictionary)
         return false;
     }
     
+    // using stat to get the size, in bytes, of the dictionary file
+    // so we know how much space to malloc and fread
     if (stat(dictionary, &test_stat)!= 0)
     {
         return false;
     }
-//    printf("Size of chain_link: %zd\n", sizeof(chain_link));
-//    printf("file size: %llu\n", (long long)test_stat.st_size);
-    
+
+    // mem_dict is the dictionary, loaded into memory - memory dictionary
     char *mem_dict;
+    
+    // allocate space for the dictionary
     if ((mem_dict = malloc((long long)test_stat.st_size + 1)) == NULL)
     {
         return false;
     }
     
+    // load the whole dictionary in one shot to avoid disk IO slowness
     if (fread(mem_dict, test_stat.st_size, 1, fp) != 1)
     {
         return false;
     }
     
+    // close the file
     fclose(fp);
 
+    
+    // this pair of pointers to char are used to navigate through mem_dict
+    // and pick out individual words
     char *where_at;
-
-    char *word_start = mem_dict;
+    char *word_start = mem_dict; // word start will be address of each word
     
-    
-//    int counter = 0;
+    // move the where_at pointer to the beginning of mem_dict
     where_at = mem_dict;
+    
+    // this loop is the guts of the load function, using the number of
+    // bytes reported in the test_stat structure as the max condition
     while (where_at < mem_dict + test_stat.st_size)
     {
+        // rawmemchar is a GNU extension - move pointer to first newline
         where_at = rawmemchr(where_at, '\n');
+        
+        // change the newline to a null terminator
         *where_at = '\0';
+        
+        // use word_start pointer to send string to hash function
         MurmurHash3_x86_32(word_start, (int)(where_at - word_start), seed, hash);
+        
+        // mod the return value from murmurhash to determine the index
         index = (hash[0]%HASH_SIZE);
+        
+        
+        // make linked list nodes, either creating or adding based on
+        // what is currently at the hashtable index
         if (hashtable[index] == NULL)
         {
             hashtable[index] = CreateLink(index, word_start);
-            actual[actual_counter] = index;
-            actual_counter++;
         }
-        else // this is a collision
+        else // this is an internal collision
         {
             hashtable[index] = AddLink(hashtable[index], index, word_start);
 //            collisions++;
         }
+        
+        // move the pointers to the presumed start of the next word 
         where_at += 1;
         word_start = where_at;
+        
+        // increment the word count
         counter++;
     }
+    
+    // free the memory occupied by mem_dict
     free(mem_dict);
-    printf("Unique Locations: %d", actual_counter);
 //    printf("Words: %d\n", counter);
 //    printf("Collisions: %d\n", collisions);
     return true;
 }
 
-inline chain_link* CreateLink(uint32_t val, const char * key)
+
+// create a new node for the linked list, inlined for performance
+inline chain_link* CreateLink(uint32_t val, char * key)
 {
     chain_link* temp = malloc(sizeof(chain_link));
     if (temp == NULL)
@@ -154,7 +186,8 @@ inline chain_link* CreateLink(uint32_t val, const char * key)
     return temp;
 }
 
-inline chain_link* AddLink(chain_link* head, uint32_t val, const char * key)
+// add a new node to an existing linked list, inlined for performance
+inline chain_link* AddLink(chain_link* head, uint32_t val, char * key)
 {
     chain_link* temp = malloc(sizeof(chain_link));
     if (temp == NULL)
@@ -189,40 +222,12 @@ bool check(const char* word)
     int index = 0;
     MurmurHash3_x86_32(temp, lenny, seed, hash);
     index = (hash[0]%HASH_SIZE);
-    if (hashtable[index] == NULL) // this is a misspelling
+    if (hashtable[index] == NULL)
     {
-        // add it to the reject array - increment uniques
-        if (unique_words[index] == NULL) // have not seen this word before
-        {
-            unique_words[index] = CreateLink(index, word); // create a chainlink for it
-            uniques++; // update uniques count
-        }
-        else // the slot in uniques is occupied - but is it the same word?
-        {
-            UniTrav = unique_words[index];
-            bool found = false;
-            while (UniTrav != NULL)
-            {
-                if (memcmp(word, (UniTrav -> key), lenny) == 0)
-                {
-                    found = true;
-                    break;
-                }
-                else
-                {
-                    UniTrav = UniTrav -> next;
-                }
-            }
-            if (found == false) // it was the same hash, but different word - update uniques
-            {
-                unique_words[index] = AddLink(unique_words[index], index, word);
-                uniques++;
-            }
-        }
         return false;
         //hashtable[index] = CreateLink(index, word_start);
     }
-    else // this is a collision with a dictionary word
+    else // this is a collision
     {
         Trav = hashtable[index];
 //        printf("%s", Trav -> key);
@@ -231,11 +236,11 @@ bool check(const char* word)
             if (memcmp(temp, (Trav -> key), lenny) == 0)
             //if (strcmp(temp, (Trav -> key)) == 0)
             {
-                return true; // found it
+                return true;
             }
             else
             {
-                Trav = Trav -> next; // keep looking
+                Trav = Trav -> next;
             }
         }
         //hashtable[index] = AddLink(hashtable[index], index, word_start);
@@ -244,57 +249,37 @@ bool check(const char* word)
     return false;
 }
 
-/**
- * Loads dictionary into memory.  Returns true if successful else false.
- */
-// bool load(const char* dictionary)
-// {
-//     if ( loader() != 0 )// TODO
-//         return false;
-//     else
-//         return true;
-// }
 
 /**
  * Returns number of words in dictionary if loaded else 0 if not yet loaded.
  */
 unsigned int size(void)
 {
-    // TODO
-    printf("Unique Mispellings: %d\n", uniques);
     return counter;
 }
 
+
 /**
- * Unloads dictionary from memory.  Returns true if successful else false.
+ * Unloads dictionary from memory by calling the destroy() function
+ * The unload function is a mandatory part of the project
  */
 bool unload(void)
 {
-    for (int i = 0; i < actual_counter; i++)
-    {
-        //actual[i];
-        destroy(hashtable[actual[i]]);
-    }
-    // destroy the uniques table
     for (int i = 0; i < HASH_SIZE; i++)
     {
-        if (unique_words[i] != NULL)
+        if (hashtable[i] != NULL)
         {
-            destroy(unique_words[i]);
+            destroy(hashtable[i]);
         }
     }
-    
-    // PRE-ACTUAL IMPLEMENTATION
-    // for (int i = 0; i < HASH_SIZE; i++)
-    // {
-    //     if (hashtable[i] != NULL)
-    //     {
-    //         destroy(hashtable[i]);
-    //     }
-    // }
     return true;
 }
 
+
+/** destroy() is a recursive function that navigates linked lists and frees
+ * each located node
+ * 
+ */
 inline void destroy(chain_link* begin)
 {
     if(begin == NULL)
@@ -304,7 +289,6 @@ inline void destroy(chain_link* begin)
     else
     {
         destroy(begin -> next);
-//        printf("free called\n");
         begin -> next = NULL;
         free(begin);
     }
