9f062b250dce12a6d23eeb6cbca6d940f6b3e85a
Ganellon <trimix@gmail.com>
2016-05-22 14:38:08 -0400

    Update speller.c

--- speller.c
+++ speller.c
@@ -123,7 +123,7 @@ int main(int argc, char* argv[])
             // print word if misspelled
             if (misspelled)
             {
-                printf("%s\n", word);
+//                printf("%s\n", word);
                 misspellings++;
             }
 
